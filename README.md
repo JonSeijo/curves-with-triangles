16dc627354a0c489461a0570456d5c26339efbbd
jonseijo <jon.seijo@gmail.com>
2017-06-10 23:42:07 -0300

    Make figures more generic

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# curves-with-triangles
 # shapes-experiment
-
-Experiment for drawing beautiful figures using triangles
-
+Creating curves through triangles superposition
 ----------
 
+![35 triangles in each segment](https://raw.githubusercontent.com/JonSeijo/curves-with-triangles/master/media/shapes_35.png)
+
+![5 triangles in each segment](https://raw.githubusercontent.com/JonSeijo/curves-with-triangles/master/media/shapes_5.png)
+
+Both images correspong with the same base figure, the diference is only the amount of triangles.
+
+---------
+
+## NOTES:
+
 Needs SFML installed for compiling
+
 https://www.sfml-dev.org
 
 Once SFML is installed, you can use the included Makefile
@@ -17,5 +25,5 @@ make
 The executable will be
 
 ```
-/triangulos
+./triangulos
 ```
--- Triangulos.cpp
+++ Triangulos.cpp
@@ -37,9 +37,10 @@ void dibujarTriangulo(punto p1, punto p2, punto p3, sf::RenderWindow &window, sf
 }
 
 // Estilo 1 del dibujo, curvas suaves
-void dibujarFigura_1(vector<punto> AB, vector<punto> BC, vector<punto> CA,
+void dibujarFigura(vector<vector<punto> > figura,
         sf::RenderWindow &window, vector<sf::Color> colorScale=vector<sf::Color>()) {
-    int n = AB.size() - 1;
+
+    int n = figura[0].size() - 1;
     int colorScaleSize = colorScale.size();
 
     if (colorScaleSize != 0 && colorScaleSize != 3*n) {
@@ -49,19 +50,20 @@ void dibujarFigura_1(vector<punto> AB, vector<punto> BC, vector<punto> CA,
 
     for (int i = 1; i <= n; i++) {
         if (colorScaleSize) {
-            dibujarTriangulo(AB[i], AB[n], BC[i], window, colorScale[i]);
-            dibujarTriangulo(BC[i], BC[n], CA[i], window, colorScale[i+1]);
-            dibujarTriangulo(CA[i], CA[n], AB[i], window, colorScale[i+2]);
+            dibujarTriangulo(figura[0][i], figura[0][n], figura[1][i], window, colorScale[i]);
+            dibujarTriangulo(figura[1][i], figura[1][n], figura[2][i], window, colorScale[i+1]);
+            dibujarTriangulo(figura[2][i], figura[2][n], figura[0][i], window, colorScale[i+2]);
         } else {
-            dibujarTriangulo(AB[i], AB[n], BC[i], window);
-            dibujarTriangulo(BC[i], BC[n], CA[i], window);
-            dibujarTriangulo(CA[i], CA[n], AB[i], window);
+            dibujarTriangulo(figura[0][i], figura[0][n], figura[1][i], window);
+            dibujarTriangulo(figura[1][i], figura[1][n], figura[2][i], window);
+            dibujarTriangulo(figura[2][i], figura[2][n], figura[0][i], window);
         }
     }
 }
 
 
-void crearPunteado(punto inicial, punto final, vector<punto> &punteado, float intervalos) {
+vector<punto> crearPunteado(punto inicial, punto final, float intervalos) {
+    vector<punto> punteado;
     punteado.push_back(inicial);
 
     punto director = final - inicial;
@@ -77,6 +79,17 @@ void crearPunteado(punto inicial, punto final, vector<punto> &punteado, float in
     }
 
     punteado.push_back(final);
+    return punteado;
+}
+
+vector<vector<punto> > crearFigura(punto a0, punto b0, punto c0, float n) {
+    vector<vector<punto> > figura(3, vector<punto>());
+
+    figura[0] = crearPunteado(a0, b0, n);
+    figura[1] = crearPunteado(b0, c0, n);
+    figura[2] = crearPunteado(c0, a0, n);
+
+    return figura;
 }
 
 int main() {
@@ -84,7 +97,7 @@ int main() {
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    sf::RenderWindow window(sf::VideoMode(404, 404), "Figuras", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(404, 404), "", sf::Style::Default, settings);
     window.setVerticalSyncEnabled(true);
     window.setFramerateLimit(20);
     sf::Color backgroundColor = sf::Color(21, 29, 45);
@@ -95,23 +108,10 @@ int main() {
     punto c0(0, 400);
     punto d0(400, 400);
 
-    vector<punto> AB;
-    vector<punto> BC;
-    vector<punto> CA;
-
-    vector<punto> BD;
-    vector<punto> DC;
-    vector<punto> CB;
-
     float n = 35;
 
-    crearPunteado(a0, b0, AB, n);
-    crearPunteado(b0, c0, BC, n);
-    crearPunteado(c0, a0, CA, n);
-
-    crearPunteado(b0, d0, BD, n);
-    crearPunteado(d0, c0, DC, n);
-    crearPunteado(c0, b0, CB, n);
+    vector<vector<punto> > figura1 = crearFigura(a0, b0, c0, n);
+    vector<vector<punto> > figura2 = crearFigura(b0, d0, c0, n);
 
     // Genero una escala de grises
     vector<sf::Color> grayScale;
@@ -129,8 +129,8 @@ int main() {
         }
         window.clear(backgroundColor);
 
-        dibujarFigura_1(AB, BC, CA, window, grayScale);
-        dibujarFigura_1(BD, DC, CB, window, grayScale);
+        dibujarFigura(figura1, window, grayScale);
+        dibujarFigura(figura2, window, grayScale);
 
         window.display();
     }
